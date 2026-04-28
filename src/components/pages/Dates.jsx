874f789bad7e832efaae641a248cbec7aa441708
Larissa Romualdo <larissamaria.romualdo@yahoo.com.br>
2026-04-28 08:29:58 -0300

    finalizando pagina details e iniciando pagina dates

--- src/components/pages/Dates.jsx
+++ src/components/pages/Dates.jsx
@@ -1,9 +1,89 @@
 import styles from './Dates.module.css'
+import { useNavigate } from 'react-router-dom'
+import Button from '../Button'
 
 function Dates() {
-     console.log("entrou na pagina")
+     
+    const navigate = useNavigate()
+
     return(
-        <div>Dates</div>
+        <div className={styles.open}>
+            <div className={styles.header}>
+            <div className={styles.title}>Dates</div>
+            </div>
+        
+            <div className={styles.border}>
+            <div className={styles.container}>
+        
+            <div className={styles.select_big}>
+            <div className={styles.select_title}>Bookable Start & End Dates</div>
+            <div className={styles.text_container}>
+                <input 
+                className={styles.text_container}
+                type="text"
+                placeholder="MM/DD/YYYY - MM/DD/YYYY"
+                />              
+            </div>
+            </div>
+
+            <div className={styles.select_big}>
+            <div className={styles.select_title}>Event Start and End Dates</div>
+                <div className={styles.text_container}>
+                <input 
+                className={styles.text_container}
+                type="text"
+                placeholder="MM/DD/YYYY - MM/DD/YYYY"
+                />              
+                </div>
+            </div>
+    
+            {/* ADD EVENT */}
+
+            <div className={styles.select_big}>
+                <div className={styles.select_title}>Default Check-In & Check-Out Dates</div>
+                <div className={styles.text_container}>
+                <input 
+                className={styles.text_container}
+                type="text"
+                placeholder="MM/DD/YYYY - MM/DD/YYYY"
+                />              
+                </div>
+            </div>
+
+            <div className={styles.select_big}>
+                <div className={styles.select_title}>Taxas & Fees</div>
+            </div>
+
+            <div className={styles.container_tree}>
+                <div className={styles.item_tree}>
+                <div className={styles.select_title}>Name</div>
+            <div className={styles.text_container}>
+                <input 
+                className={styles.text_container_tree}
+                placeholder="Type here"
+                type="text"
+                />
+            </div>
+            </div>
+            </div>
+        </div>
+
+            <div className={styles.controls}>
+                <button className={styles.navbutton} onClick={() => navigate('/details')}>
+                <img src="/arrow-left.svg" alt="Anterior" />
+                </button>
+                <button className={styles.navbutton}>
+                <img src="/arrow-right-gray.svg" alt="right" />
+                </button>
+            </div>
+            <div className={styles.save}>
+                <Button 
+                    text="Save" 
+                    onClick={() => alert('salvo')}
+                />
+            </div>  
+       </div>
+       </div> 
        
     )
 }
--- src/components/pages/Details.jsx
+++ src/components/pages/Details.jsx
@@ -8,18 +8,85 @@ function Details() {
    
     return(
         <div className={styles.open}>
-        <div className={styles.header}>
+            <div className={styles.header}>
             <div className={styles.title}>Details</div>
+            </div>
+
+            <div className={styles.border}>
+            <div className={styles.container}>
+
+            <div className={styles.select_big}>
+            <div className={styles.select_title}>Link</div>
+            <div className={styles.text_container}>
+                <input 
+                className={styles.text_container}
+                 placeholder="Enter Link"
+                type="text"
+                />              
+            </div>
+            </div>
+
+            <div className={styles.select}>
+            <div className={styles.select_title}>Event Address</div>
+            <div className={styles.text_container}>
+                <input 
+                className={styles.text_container}
+                 placeholder="Enter Event Address"
+                type="text"
+                />              
+            </div>
+            </div>
+
+            <div className={styles.select}>
+            <div className={styles.select_title}>Venue Name</div>
+            <div className={styles.text_container}>
+                <input 
+                className={styles.text_container}
+                 placeholder="Enter Venue Name"
+                type="text"
+                />              
+            </div>
+            </div>
+
+            <div className={styles.select}>
+            <div className={styles.select_title}>Feature Hotels Title</div>
+            <div className={styles.text_container}>
+                <input 
+                className={styles.text_container}
+                 placeholder="Enter Feature Hotels Title"
+                type="text"
+                />              
+            </div>
+            </div>
+
+            <div className={styles.select}>
+            <div className={styles.select_title}>Minimum Nights</div>
+            <div className={styles.text_container}>
+                <input 
+                className={styles.text_container}
+                type="number"
+                placeholder="Enter Minimum Nights"
+                min="1"
+                step="1"
+                value="1"
+                /> 
+
+                <div>
+                <button type="button" className={styles.button_on}>
+                    <img className={styles.button_on}
+                    src='/up-arrow.svg' />
+                </button>  
+                <button type="button" className={styles.button_down}>
+                    <img className={styles.button_down}
+                     src='/arrow-down.svg' />
+                </button>   
+                </div>          
+            </div>
+            </div>
+        </div>
         </div>
 
-        <div className={styles.border}>
-        <div className={styles.container}></div>
-
-
-        </div>
-        
-
-         <div className={styles.controls}>
+        <div className={styles.controls}>
                 <button className={styles.navbutton} onClick={() => navigate('/')}>
                 <img src="/arrow-left.svg" alt="Anterior" />
                 </button>
