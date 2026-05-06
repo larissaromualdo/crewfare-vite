eae529c27504bc3c9ac3c2d58f882d697e4edad5
Larissa Romualdo <larissamaria.romualdo@yahoo.com.br>
2026-05-06 09:55:51 -0300

    dados terceira pagina

--- src/components/organisms/Dates.jsx
+++ src/components/organisms/Dates.jsx
@@ -1,10 +1,14 @@
 import styles from './Dates.module.css'
 import { useNavigate } from 'react-router-dom'
 import Button from '../atoms/Button'
+import NumberButton from '../atoms/NumberButton'
+import { useState } from 'react'
 
 function Dates() {
      
     const navigate = useNavigate()
+     const [minNights, setMinNights] = useState(1)
+
 
     return(
     <div className={styles.conteudo}>
@@ -74,7 +78,52 @@ function Dates() {
                 </div>
             </div>
 
-        </div>
+             <div className={styles.title_dates}>
+                    <div className={styles.select_title}>Taxes & Fees</div>
+                </div>
+
+
+            <div className={styles.container_tree}>
+
+            <div className={styles.item_tree}>
+                <div className={styles.select_title}>Name</div>
+            
+                <div className={styles.text_container_tree}>
+                <input
+                className={styles.input_text}
+                placeholder="Type here"
+                type="text"
+                />
+            </div>
+            </div>
+
+
+            <div className={styles.item_four}>
+                <div className={styles.select_title}>Amount</div>
+            
+                <div className={styles.text_container_four}>
+                <NumberButton
+                value={minNights}
+                setValue={setMinNights}
+                />
+            </div>
+            </div>
+           
+           <div className={styles.item_four}>
+                <div className={styles.select_title}>Type</div>
+            
+                <div className={styles.text_container_four}>
+                <input
+                className={styles.input_text}
+                placeholder="Fixed"
+                type="text"
+                />
+            </div>
+            </div>
+            
+            </div>
+            </div>
+
         </div>
     </div>
             <div className={styles.controls}>
